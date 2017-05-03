23c34e83e83e41b414026c7537e96431b84faac7
shahsk0901 <shah.sk0901@gmail.com>
2017-05-03 12:00:47 -0500

    analyzed and corrected flaw in algorithm, when one number repeated more than 2 times, was stuck in infinite loop

--- src/daa/QuickSortFixed.java
+++ src/daa/QuickSortFixed.java
@@ -5,7 +5,6 @@ import java.util.*;
 public class QuickSortFixed {
 
     static Double[] arr;
-
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
@@ -20,15 +19,16 @@ public class QuickSortFixed {
 
     	generateArray(inputSize);
 
+    	//Integer[] arr = {147,287,889,277, 873, 119, 815, 550, 424, 668, 137, 814, 933, 661, 557, 180, 168, 733, 89, 121, 250,12, 595, 916, 889, 139, 964, 635, 180, 75,954,26,93,888,1000,1,17,99,150,77,31,44,55,20};
+        //Double[] arr = {10.0,9.0,8.0,7.0,6.0,5.0,4.0,3.0,2.0,1.0,0.0,1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0,22.0};
+        //Integer[] arr = {52,26,85,10,97,1};
+        //Integer[] arr = {63, 98, 38,1,80,2};
+        //Double[] arr = {1.0,10.0,1.0,1.0,1.0,1.0,1.0,2.0};
+    	
     	System.out.println("Initial Array:");
     	for(int i=0;i<arr.length;i++) {
     	    System.out.print(arr[i] + " ");
-        }
-
-        //Integer[] arr = {147,287,889,277, 873, 119, 815, 550, 424, 668, 137, 814, 933, 661, 557, 180, 168, 733, 89, 121, 250,12, 595, 916, 889, 139, 964, 635, 180, 75,954,26,93,888,1000,1,17,99,150,77,31,44,55,20};
-        //Integer[] arr = {10,9,8,7,6,5,4,3,2,1,0,1,2,3,4,5,6,7,8,9,10,22};
-        //Integer[] arr = {52,26,85,10,97,1};
-        //Integer[] arr = {63, 98, 38,1,80,2};
+    	}
 
         quickSort(arr,0,arr.length-1);
         System.out.println("\nSorted Array:");
@@ -62,7 +62,7 @@ public class QuickSortFixed {
         Integer last = end;
 
         while(first<last) {
-            while(arr[first]<arr[pivot] && first < last) {
+            while(arr[first]<=arr[pivot] && first < last) {
                 first++;
             }
             while(arr[last]>arr[pivot]) {
